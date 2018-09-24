1f32239059f14e267700ffa7873b764189c2b45f
RogerSoleForm3 <roger.sole@form3.tech>
2018-09-24 12:49:11 +0200

    Brew formula update for pd-report version v1.3.1

--- Formula/pd-report.rb
+++ Formula/pd-report.rb
@@ -1,9 +1,9 @@
 class PdReport < Formula
   desc "Get a report of oncall rotations from your PagerDuty account"
   homepage "https://github.com/form3tech-oss/go-pagerduty-oncall-report/"
-  url "https://github.com/form3tech-oss/go-pagerduty-oncall-report/releases/download/v1.3.0/pd-report_1.3.0_darwin_amd64.tar.gz"
-  version "1.3.0"
-  sha256 "523853d9a00099a4e262f08aa8a91963b784a7267d2ab54d40cc4d6e06c47fd3"
+  url "https://github.com/form3tech-oss/go-pagerduty-oncall-report/releases/download/v1.3.1/pd-report_1.3.1_darwin_amd64.tar.gz"
+  version "1.3.1"
+  sha256 "eff7a0a66fe7b70ea7cb3e1ab7990713ad5e1f174d0093803763d47feafc7fbe"
 
   def install
     bin.install "pd-report"
